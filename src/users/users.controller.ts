5feaf5a4362709513eedc1e92fa04e2493695ed7
KovacevicAleksa <aleksakovacevicc@gmail.com>
2024-06-28 13:25:57 +0200

    asynchronous architecture

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -19,54 +19,44 @@ import { AdminGuard } from 'src/admin/admin.guard';
 
 @Controller('users')
 export class UsersController {
-  //GET /users?level=vip -> [ ... , ...]
   constructor(private readonly usersService: UsersService) {}
+
   @Get()
-  getUsers(@Query('level') level?: 'premium' | 'vip') {
-    return this.usersService.getUsers(level);
+  async getUsers(@Query('level') level?: 'premium' | 'vip') {
+    return await this.usersService.getUsers(level);
   }
 
-  //GET /users/:id -> {}
   @Get(':id')
-  getOneUsers(@Param('id', ParseIntPipe) id: number) {
+  async getOneUsers(@Param('id', ParseIntPipe) id: number) {
     try {
-      return this.usersService.getUser(id);
+      return await this.usersService.getUser(id);
     } catch (error) {
       throw new NotFoundException();
     }
   }
 
-  //Post /users -> {}
   @UseGuards(AdminGuard)
   @Post('')
-  createUsers(@Body(new ValidationPipe()) createUserDto: CreateUserDto) {
+  async createUsers(@Body(new ValidationPipe()) createUserDto: CreateUserDto) {
     try {
-      return this.usersService.createUser(createUserDto);
+      return await this.usersService.createUser(createUserDto);
     } catch (error) {
       throw new NotFoundException();
     }
   }
 
-  //Put /users/:id -> {}
   @Put(':id')
-  updateUsers(
+  async updateUsers(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateUserDto: UpdateUserDto,
   ) {
-    return this.usersService.updateUser(id, updateUserDto);
+    return await this.usersService.updateUser(id, updateUserDto);
   }
 
-  //   //Patch /users/:id -> {}
-  //   @Patch(':id')
-  //   updateAttributeUsers() {
-  //     return {};
-  //   }
-
-  //Delete /users/:id -> {}
   @Delete(':id')
-  removeUsers(@Param('id', ParseIntPipe) id: number) {
+  async removeUsers(@Param('id', ParseIntPipe) id: number) {
     try {
-      return this.usersService.removeUser(id);
+      return await this.usersService.removeUser(id);
     } catch (error) {
       throw new NotFoundException();
     }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -13,47 +13,43 @@ export class UsersService {
     { id: 1, name: 'VipKorisnik', level: 'vip' },
   ];
 
-  getUsers(level?: 'premium' | 'vip') {
+  async getUsers(level?: 'premium' | 'vip'): Promise<any[]> {
     if (level) {
       return this.users.filter((user) => user.level === level);
     }
     return this.users;
   }
 
-  getUser(id?: number) {
-    const user = this.users.filter((user) => user.id === id);
+  async getUser(id?: number): Promise<any> {
+    const user = this.users.find((user) => user.id === id);
     if (!user) {
       throw new Error('User not found');
     }
     return user;
   }
 
-  createUser(createUserDto: CreateUserDto) {
+  async createUser(createUserDto: CreateUserDto): Promise<any> {
     const newUser = {
       ...createUserDto,
       id: Date.now(),
     };
     this.users.push(newUser);
-
     return newUser;
   }
 
-  updateUser(id: number, updateUserDto: UpdateUserDto) {
+  async updateUser(id: number, updateUserDto: UpdateUserDto): Promise<any> {
     this.users = this.users.map((user) => {
       if (user.id === id) {
         return { ...user, ...updateUserDto };
       }
-
       return user;
     });
     return this.getUser(id);
   }
 
-  removeUser(id: number) {
-    const toBeRemoved = this.getUser(id);
-
+  async removeUser(id: number): Promise<any> {
+    const toBeRemoved = await this.getUser(id);
     this.users = this.users.filter((user) => user.id !== id);
-
     return toBeRemoved;
   }
 }
